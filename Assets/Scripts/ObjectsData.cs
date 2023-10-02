e18fe2cb150dee3e989842f8344b2b1b90c498fe
Mario Andrés Acevedo Pico <acevedopico@hotmail.com>
2023-10-02 15:18:32 -0600

    Jose con esqueleto

---
+++ Assets/Scripts/ObjectsData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsData : MonoBehaviour
+{
+    public float ObjectScale;
+
+    public float ObjectOrigiaScale;
+}
