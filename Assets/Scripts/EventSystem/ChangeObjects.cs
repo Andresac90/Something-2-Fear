a6ba7e9b0e44c08689961769a6139e6658be51ff
Alex <A00825543@itesm.mx>
2023-10-31 12:59:32 -0600

    Event Lockdown Zero

--- Assets/Scripts/EventSystem/ChangeObjects.cs
+++ Assets/Scripts/EventSystem/ChangeObjects.cs
@@ -11,39 +11,66 @@ public class ChangeObjects : MonoBehaviourPun
     private GameObject dummy;
     [SerializeField] 
     private GameObject lights;
+    [SerializeField]
+    private GameObject Nurse;
+    [SerializeField]
+    private GameObject HospitalDoor;
+    [SerializeField]
+    private GameObject HospitalLightsLockdown;
 
     [PunRPC]
     public void ActivatePascualita()
     {
-        bool value = pascualita.activeInHierarchy;
         pascualita.SetActive(true);
     }
 
     [PunRPC]
     public void DeactivatePascualita()
     {
-        bool value = pascualita.activeInHierarchy;
         pascualita.SetActive(false);
     }
 
     [PunRPC]
     public void DeactivateDummy()
     {
-        bool value = dummy.activeInHierarchy;
         dummy.SetActive(false);
     }
 
     [PunRPC]
     public void ActivateLights()
     {
-        bool value = lights.activeInHierarchy;
         lights.SetActive(true);
     }
 
     [PunRPC]
     public void DeactivateLights()
     {
-        bool value = lights.activeInHierarchy;
         lights.SetActive(false);
+    }
+
+    [PunRPC]
+    public void ActivateNurse()
+    {
+        Nurse.SetActive(true);
+        HospitalLightsLockdown.SetActive(false);
+    }
+
+    [PunRPC]
+    public void DeactivateNurse()
+    {
+        Nurse.SetActive(false);
+    }
+
+    [PunRPC]
+    public void ActivateLockdown()
+    {
+        HospitalLightsLockdown.SetActive(true);
+        HospitalDoor.GetComponent<PhotonView>().RPC("SyncDoor", RpcTarget.All, false);
+    }
+
+    [PunRPC]
+    public void DeactivateLockdown()
+    {
+        Nurse.SetActive(false);
     }
 }
