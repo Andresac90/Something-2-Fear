69d76ba1f086b59cc5c9f002164ebe7d6a4f70d2
Alex <A00825543@itesm.mx>
2023-11-09 21:50:24 -0600

    1

--- Assets/Scripts/Minigames/Labyrinth.cs
+++ Assets/Scripts/Minigames/Labyrinth.cs
@@ -10,14 +10,14 @@ public class Labyrinth : MonoBehaviour
     private GameObject Puzzle; //terminal
     private Puzzle Comprobations; //iteraciones de puertas
     public GameObject[] doorsList; //lista de puertas
-    private GameObject Nina;
+    public GameObject Nina;
 
     void Start()
     {
         doorsList = new GameObject[5];
         Passcodes = new string[5];
         Puzzle = GameObject.Find("Labyrinth MiniGame");
-        Nina = GameObject.Find("Nina");
+        Nina = GameObject.Find("Niña");
         Comprobations = Puzzle.GetComponent<Puzzle>();
         for (int i = 0; i < doorsList.Length; i++)
         {
@@ -29,6 +29,7 @@ public class Labyrinth : MonoBehaviour
             Passcodes[i] += doorsList[i].GetComponent<LabDoor>().Code;
             Debug.Log(Passcodes[i]);
         }
+        if (Nina == null) { Debug.Log("Null"); }
     }
 
     public void AddSymbol(string symbol)
