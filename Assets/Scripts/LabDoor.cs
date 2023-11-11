f7d926b642916c71c1e184f84fd310eedb6c1655
Alex <A00825543@itesm.mx>
2023-11-10 19:02:07 -0600

    Symbols Attempt 1

--- Assets/Scripts/LabDoor.cs
+++ Assets/Scripts/LabDoor.cs
@@ -9,6 +9,8 @@ public class LabDoor : MonoBehaviour
     public string Code = "";
     private int iCode;
     private string[] Symbols = { "t", "s", "c", "p" };
+    public GameObject visions;
+    public GameObject[] symbols;
 
     // Start is called before the first frame update
     void Start()
@@ -28,5 +30,18 @@ public class LabDoor : MonoBehaviour
     {
         iCode = iC;
         Code = C;
+        for (int i = 0; i < Code.Length; i++)
+        {
+            for (int j = 0; j < symbols.Length; j++)
+            {
+                if (Code[i].ToString() == symbols[j].name)
+                {
+                    GameObject spawnedPrefab = Instantiate(symbols[j], visions.transform.GetChild(i).position, Quaternion.identity);
+                    spawnedPrefab.transform.parent = visions.transform.GetChild(i);  // Set the parent to visions' child
+                }
+            }
+        }
     }
+
+
 }
