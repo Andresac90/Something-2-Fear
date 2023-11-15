b916887968d4ca3fa242ea88a25e7b6e3b416198
Alex <A00825543@itesm.mx>
2023-11-14 18:13:09 -0600

    Update LabDoor.cs

--- Assets/Scripts/LabDoor.cs
+++ Assets/Scripts/LabDoor.cs
@@ -39,7 +39,7 @@ public class LabDoor : MonoBehaviour
                     Debug.Log(symbols[j].name);
                     GameObject spawnedPrefab = Instantiate(symbols[j], visions.transform.GetChild(i).position, Quaternion.identity);
                     spawnedPrefab.transform.parent = visions.transform.GetChild(i);  // Set the parent to visions' child
-                    spawnedPrefab.transform.rotation = Quaternion.identity;  // Set rotation to zero
+                    spawnedPrefab.transform.rotation = ;  // Set rotation to zero
                     spawnedPrefab.transform.localScale = Vector3.one;  // Set scale to (1, 1, 1)
                 }
             }
