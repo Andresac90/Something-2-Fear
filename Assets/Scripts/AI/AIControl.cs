6567d4f9ebe191a2e2be2d6a1cc1a3cb1abecbab
José Andrés Lozano <andreslozano00@hotmail.com>
2023-11-30 01:12:08 -0600

    Update AIControl.cs

--- Assets/Scripts/AI/AIControl.cs
+++ Assets/Scripts/AI/AIControl.cs
@@ -450,7 +450,7 @@ public class AIControl : MonoBehaviourPun
         aiAnimation.SetBool("stop", false);
         aiAnimation.ResetTrigger("attack");
         aiAnimation.SetTrigger("pose1");
-        if (SantiPV.IsMine)
+        if (!JosePV.IsMine)
         {
             SantiPV.RPC("ResetJumpScareAnimationSanti", RpcTarget.All);
         }
@@ -464,7 +464,7 @@ public class AIControl : MonoBehaviourPun
         aiAnimation.SetBool("stop", false);
         aiAnimation.ResetTrigger("attack");
         aiAnimation.SetTrigger("pose1");
-        if (JosePV.IsMine)
+        if (!SantiPV.IsMine)
         {
             JosePV.RPC("ResetJumpScareAnimationSanti", RpcTarget.All);
         }
